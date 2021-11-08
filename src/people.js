52197f592aa04663b69d2e19525a5c72ceeb0a20
Ramya Pallu <ramyapallu12@.com>
2021-11-08 03:48:15 -0800

    first commit

---
+++ src/people.js
@@ -0,0 +1,12 @@
+/*
+function getHello(){
+    return 'Hello There';
+}
+*/
+
+let people = [
+    {name: 'John Doe'},
+    {name: 'Steve Smith'},
+    {name: 'Carol Williams'}
+];
+module.exports = people;
